ecaea1445287f9e48a0c0f7cdccefa68ad75f35b
Chris Lundquist <rampantdurandal@gmail.com>
2010-03-26 19:26:47 -0700

    rubified and beautified the lesson

--- lesson05.rb
+++ lesson05.rb
@@ -7,6 +7,12 @@ require "opengl"
 require "glut"
 
 
+# Rotation angle for the triangle.
+$rtri=0.0
+
+#Rotation angle for the quadrilateral.
+$rquad=0.0
+
 # A general OpenGL initialization function.  Sets all of the initial parameters
 
 def InitGL(width, height) # We call this right after our OpenGL window 
@@ -27,10 +33,10 @@ end
 
 # The function called when our window is resized (which shouldn't happen, 
 # because we're fullscreen) 
-ReSizeGLScene = Proc.new {|width, height|
-  if (height==0) # Prevent A Divide By Zero If The Window Is Too Small
-    height=1
-  end
+resize_gl_scene = Proc.new {|width, height|
+ # Prevent A Divide By Zero If The Window Is Too Small
+   height = 1 if height == 0
+  
   GL.Viewport(0,0,width,height) # Reset The Current Viewport And
                                 # Perspective Transformation
   GL.MatrixMode(GL::PROJECTION)
@@ -40,7 +46,7 @@ ReSizeGLScene = Proc.new {|width, height|
 }
 
 # The main drawing function. 
-DrawGLScene = Proc.new {
+draw_gl_scene = Proc.new {
   GL.Clear(GL::COLOR_BUFFER_BIT | GL::DEPTH_BUFFER_BIT) # Clear The Screen And
                                           # The Depth Buffer
   GL.LoadIdentity()                       # Reset The View
@@ -50,96 +56,94 @@ DrawGLScene = Proc.new {
 
   # draw a triangle (in smooth coloring mode)
   GL.Begin(GL::POLYGON)                   # start drawing a polygon
+    # front face of pyramid
+    GL.Color3f(  1.0, 0.0, 0.0)           # Set The Color To Red
+    GL.Vertex3f( 0.0, 1.0, 0.0)           # Top
+    GL.Color3f(  0.0, 1.0, 0.0)           # Set The Color To Green
+    GL.Vertex3f( -1.0,-1.0, 1.0)          # Bottom Right
+    GL.Color3f(  0.0, 0.0, 1.0)           # Set The Color To Blue
+    GL.Vertex3f(1.0,-1.0, 1.0)            # Bottom Left  
 
-  # front face of pyramid
-  GL.Color3f(  1.0, 0.0, 0.0)             # Set The Color To Red
-  GL.Vertex3f( 0.0, 1.0, 0.0)             # Top
-  GL.Color3f(  0.0, 1.0, 0.0)             # Set The Color To Green
-  GL.Vertex3f( -1.0,-1.0, 1.0)             # Bottom Right
-  GL.Color3f(  0.0, 0.0, 1.0)             # Set The Color To Blue
-  GL.Vertex3f(1.0,-1.0, 1.0)             # Bottom Left  
+    # right face of pyramid
+    GL.Color3f(  1.0, 0.0, 0.0)           # Red
+    GL.Vertex3f( 0.0, 1.0, 0.0)           # Top of triangle (Right)
+    GL.Color3f(  0.0, 0.0, 1.0)           # Blue
+    GL.Vertex3f( 1.0,-1.0, 1.0)           # Left of triangle (Right)
+    GL.Color3f(  0.0, 1.0, 0.0)           # Green
+    GL.Vertex3f( 1.0,-1.0,-1.0)           # Right of triangle (Right)
 
-  # right face of pyramid
-  GL.Color3f(  1.0, 0.0, 0.0)             # Red
-  GL.Vertex3f( 0.0, 1.0, 0.0)             # Top of triangle (Right)
-  GL.Color3f(  0.0, 0.0, 1.0)             # Blue
-  GL.Vertex3f( 1.0,-1.0, 1.0)             # Left of triangle (Right)
-  GL.Color3f(  0.0, 1.0, 0.0)             # Green
-  GL.Vertex3f( 1.0,-1.0,-1.0)             # Right of triangle (Right)
+    # back face of pyramid
+    GL.Color3f(  1.0, 0.0, 0.0)           # Red
+    GL.Vertex3f( 0.0, 1.0, 0.0)           # Top of triangle (Back)
+    GL.Color3f(  0.0, 1.0, 0.0)           # Green
+    GL.Vertex3f( 1.0,-1.0,-1.0)           # Left of triangle (Back)
+    GL.Color3f(  0.0, 0.0, 1.0)           # Blue
+    GL.Vertex3f(-1.0,-1.0,-1.0)           # Right of triangle (Back
 
-  # back face of pyramid
-  GL.Color3f(  1.0, 0.0, 0.0)             # Red
-  GL.Vertex3f( 0.0, 1.0, 0.0)             # Top of triangle (Back)
-  GL.Color3f(  0.0, 1.0, 0.0)             # Green
-  GL.Vertex3f( 1.0,-1.0,-1.0)             # Left of triangle (Back)
-  GL.Color3f(  0.0, 0.0, 1.0)             # Blue
-  GL.Vertex3f(-1.0,-1.0,-1.0)             # Right of triangle (Back
-
-  # left face of pyramid
-  GL.Color3f(  1.0, 0.0, 0.0)             # Red
-  GL.Vertex3f( 0.0, 1.0, 0.0)             # Top of triangle (Left)
-  GL.Color3f(  0.0, 0.0, 1.0)             # Blue
-  GL.Vertex3f(-1.0,-1.0,-1.0)             # Left of triangle (Left)
-  GL.Color3f(  0.0, 1.0, 0.0)             # Green
-  GL.Vertex3f(-1.0,-1.0, 1.0)             # Right of triangle (Left)
-
+    # left face of pyramid
+    GL.Color3f(  1.0, 0.0, 0.0)           # Red
+    GL.Vertex3f( 0.0, 1.0, 0.0)           # Top of triangle (Left)
+    GL.Color3f(  0.0, 0.0, 1.0)           # Blue
+    GL.Vertex3f(-1.0,-1.0,-1.0)           # Left of triangle (Left)
+    GL.Color3f(  0.0, 1.0, 0.0)           # Green
+    GL.Vertex3f(-1.0,-1.0, 1.0)           # Right of triangle (Left)
   GL.End()                                # Done drawing the pyramid
 
   GL.LoadIdentity()                       # make sure we're no longer rotated.
   GL.Translate(1.5,0.0,-7.0)              # Move Right 3 Units, and back into 
                                           # the screen 7.0
   GL.Rotate($rquad,1.0,1.0,1.0)           # Rotate the quad on the X Axis
-  # draw a cube (6 quadrilateral)
+  # Draw a cube (6 quadrilateral)
   GL.Begin(GL::QUADS)                     # start drawing the cube
 
-  # top of cube
-  GL.Color3f(0.0,1.0,0.0)               # Set The Color To Blue
+  # Top of cube
+  GL.Color3f(0.0,1.0,0.0)                 # Set The Color To Blue
   GL.Vertex3f( 1.0, 1.0,-1.0)             # Top Right Of The Quad (Top)
   GL.Vertex3f(-1.0, 1.0,-1.0)             # Top Left Of The Quad (Top)
   GL.Vertex3f(-1.0, 1.0, 1.0)             # Bottom Left Of The Quad (Top)
   GL.Vertex3f( 1.0, 1.0, 1.0)             # Bottom Right Of The Quad (Top)
 
-  # bottom of cube
-  GL.Color3f(1.0,0.5,0.0)             	# Set The Color To Orange
+  # Bottom of cube
+  GL.Color3f(1.0,0.5,0.0)                 # Set The Color To Orange
   GL.Vertex3f( 1.0,-1.0, 1.0)             # Top Right Of The Quad (Bottom)
   GL.Vertex3f(-1.0,-1.0, 1.0)             # Top Left Of The Quad (Bottom)
   GL.Vertex3f(-1.0,-1.0,-1.0)             # Bottom Left Of The Quad (Bottom)
   GL.Vertex3f( 1.0,-1.0,-1.0)             # Bottom Right Of The Quad (Bottom)
 
-  # front of cube
-  GL.Color3f(1.0,0.0,0.0)             	# Set The Color To Red
+  # Front of cube
+  GL.Color3f(1.0,0.0,0.0)                 # Set The Color To Red
   GL.Vertex3f( 1.0, 1.0, 1.0)             # Top Right Of The Quad (Front)
   GL.Vertex3f(-1.0, 1.0, 1.0)             # Top Left Of The Quad (Front)
   GL.Vertex3f(-1.0,-1.0, 1.0)             # Bottom Left Of The Quad (Front)
   GL.Vertex3f( 1.0,-1.0, 1.0)             # Bottom Right Of The Quad (Front)
 
-  # back of cube.
-  GL.Color3f(1.0,1.0,0.0)             	# Set The Color To Yellow
+  # Back of cube.
+  GL.Color3f(1.0,1.0,0.0)                 # Set The Color To Yellow
   GL.Vertex3f( 1.0,-1.0,-1.0)             # Top Right Of The Quad (Back)
   GL.Vertex3f(-1.0,-1.0,-1.0)             # Top Left Of The Quad (Back)
   GL.Vertex3f(-1.0, 1.0,-1.0)             # Bottom Left Of The Quad (Back)
   GL.Vertex3f( 1.0, 1.0,-1.0)             # Bottom Right Of The Quad (Back)
 
-  # left of cube
-  GL.Color3f(0.0,0.0,1.0)             	# Blue
+  # Left of cube
+  GL.Color3f(0.0,0.0,1.0)                 # Blue
   GL.Vertex3f(-1.0, 1.0, 1.0)             # Top Right Of The Quad (Left)
   GL.Vertex3f(-1.0, 1.0,-1.0)             # Top Left Of The Quad (Left)
   GL.Vertex3f(-1.0,-1.0,-1.0)             # Bottom Left Of The Quad (Left)
   GL.Vertex3f(-1.0,-1.0, 1.0)             # Bottom Right Of The Quad (Left)
 
   # Right of cube
-  GL.Color3f(1.0,0.0,1.0)             	# Set The Color To Violet
-  GL.Vertex3f( 1.0, 1.0,-1.0);	          # Top Right Of The Quad (Right)
+  GL.Color3f(1.0,0.0,1.0)                 # Set The Color To Violet
+  GL.Vertex3f( 1.0, 1.0,-1.0);            # Top Right Of The Quad (Right)
   GL.Vertex3f( 1.0, 1.0, 1.0)             # Top Left Of The Quad (Right)
   GL.Vertex3f( 1.0,-1.0, 1.0)             # Bottom Left Of The Quad (Right)
   GL.Vertex3f( 1.0,-1.0,-1.0)             # Bottom Right Of The Quad (Right)
   GL.End();                               # done with the polygon
 
-  $rtri += 0.05                        # Increase the rotation variable for
+  $rtri += 0.05                           # Increase the rotation variable for
                                           # the Triangle
-  $rquad -= 0.05                      # Decrease the rotation variable for 
+  $rquad -= 0.05                          # Decrease the rotation variable for 
                                           # the Quad
-  # we need to swap the buffer to display our drawing.
+  # We need to swap the buffer to display our drawing.
   GLUT.SwapBuffers();
 }
 
@@ -148,31 +152,27 @@ DrawGLScene = Proc.new {
 # The function called whenever a key is pressed.
 keyPressed = Proc.new {|key, x, y| 
 
-  # If escape is pressed, kill everything. 
-  if (key == 27) 
-    # shut down our window 
+  case key
+  when 27
+    # If escape is pressed, kill everything and shut down our window.
     GLUT.DestroyWindow($window)
     # exit the program...normal termination.
-    exit(0)                   
+    exit(0)
+  else
+    # Do Nothing
   end
 }
 
-# Rotation angle for the triangle.
-$rtri=0.0
-
-#Rotation angle for the quadrilateral.
-$rquad=0.0
-
 #Initialize GLUT state - glut will take any command line arguments that pertain
-# to it or X Windows - look at its documentation at 
-# http://reality.sgi.com/mjk/spec3/spec3.html 
+# to it or X Windows - look at its documentation at
+# http://reality.sgi.com/mjk/spec3/spec3.html
 GLUT.Init
 
-#Select type of Display mode:   
+#Select type of Display mode:
 # Double buffer 
 # RGBA color
 # Alpha components supported 
-# Depth buffer 
+# Depth buffer
 GLUT.InitDisplayMode(GLUT::RGBA|GLUT::DOUBLE|GLUT::ALPHA|GLUT::DEPTH)
 
 # get a 640x480 window
@@ -182,22 +182,22 @@ GLUT.InitWindowSize(640,480)
 GLUT.InitWindowPosition(0,0)
 
 # Open a window
-$window=GLUT.CreateWindow("Jeff Molofee's GL Code Tutorial ... NeHe '99")
+$window = GLUT.CreateWindow("Jeff Molofee's GL Code Tutorial ... NeHe '99")
 
 # Register the function to do all our OpenGL drawing.
-GLUT.DisplayFunc(DrawGLScene)
+GLUT.DisplayFunc(draw_gl_scene)
 
 # Go fullscreen. This is as soon as possible.
 GLUT.FullScreen()
 
 # Even if there are no events, redraw our gl scene.
-GLUT.IdleFunc(DrawGLScene)
+GLUT.IdleFunc(draw_gl_scene)
 
 # Register the function called when our window is resized.
-GLUT.ReshapeFunc(ReSizeGLScene)
+GLUT.ReshapeFunc(resize_gl_scene)
 
 # Register the function called when the keyboard is pressed.
-GLUT.KeyboardFunc(keyPressed)
+GLUT.KeyboardFunc(key_pressed)
 
 # Initialize our window.
 InitGL(640, 480)
